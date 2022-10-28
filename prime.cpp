16cd843d70d48c918c96b92ec13e535a0064cf4e
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-10-29 00:22:47 +0600

    add format

--- prime.cpp
+++ prime.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 bool isPrime(int n) {
     // Corner case
-    if(n <= 1)
+    if (n <= 1) {
         return false;
-
+    }
     // Check from 2 to n-1
-    for(int i = 2; i < n; i++)
-        if(n % i == 0)
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
             return false;
-
+        }
+    }
     return true;
 }
 
